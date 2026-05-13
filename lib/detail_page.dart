312fe0c47442948dc661504393a55831a89db18f
yamazaki souhei <kisaragi.03285656@gmail.com>
2026-05-13 16:17:57 +0900

    Add tourist spot list

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
+  final String spotName;
+
+  const DetailPage({
+    super.key,
+    required this.spotName,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('詳細')),
+      appBar: AppBar(
+        title: Text(spotName),
+      ),
+
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('戻る'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            Text(
+              spotName,
+              style: const TextStyle(
+                fontSize: 30,
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            const Text(
+              '観光地の詳細情報を表示するページです。',
+            ),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+
+              child: const Text('戻る'),
+            ),
+          ],
         ),
       ),
     );
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,22 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:simple_navigation_mock/detail_page.dart';
 
 class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  // 観光地リスト
+  final List<String> touristSpots = [
+    '鶴ヶ城',
+    '大内宿',
+    '猪苗代湖',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DetailPage(),
-              ),
-            );
-          },
-          child: const Text('詳細画面へ'),
-        ),
+      appBar: AppBar(
+        title: const Text('Fukushima Navi'),
+      ),
+
+      body: ListView.builder(
+        itemCount: touristSpots.length,
+
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(touristSpots[index]),
+
+              onTap: () {
+                Navigator.push(
+                  context,
+
+                  MaterialPageRoute(
+                    builder: (context) => DetailPage(
+                      spotName: touristSpots[index],
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
