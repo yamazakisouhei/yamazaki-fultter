38331c8dac1037e7b5dc78b7215cda0425f9d578
yamazaki souhei <kisaragi.03285656@gmail.com>
2026-05-27 15:48:27 +0900

    観光地ごとの画像を追加

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,12 +5,20 @@ class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   // 観光地リスト
-  final List<String> touristSpots = [
-    '鶴ヶ城',
-    '大内宿',
-    '猪苗代湖',
-  ];
-
+  final List<Map<String, String>> touristSpots = [
+  {
+    'name': '鶴ヶ城',
+    'image': 'assets/images/turugazyou.jpg',
+  },
+  {
+    'name': '大内宿',
+    'image': 'assets/images/ouchijuku.jpg',
+  },
+  {
+    'name': '猪苗代湖',
+    'image': 'assets/images/inawasiroko.jpg',
+  },
+];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,20 +33,20 @@ class HomePage extends StatelessWidget {
           return Card(
             child: ListTile(
               leading: Image.asset(
-                'assets/images/turugazyou.jpg',
+                touristSpots[index]['image']!,
                 width: 60,
                 height: 60,
                 fit: BoxFit.cover,
               ),
 
-              title: Text(touristSpots[index]),
+              title: Text(touristSpots[index]['name']!),
 
               onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) => DetailPage(
-                      spotName: touristSpots[index],
+                     spotName: touristSpots[index]['name']!,
                     ),
                   ),
                 );
